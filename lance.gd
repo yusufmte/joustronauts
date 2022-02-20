e9421b476efde74936b15d25b03ab7b67a857e67
Yusuf Ebrahim <yusufmte@gmail.com>
2022-02-20 15:42:40 -0500

    create lance.gd, set up movement variables and keys

---
+++ lance.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+export var acceleration = 400
+export var max_speed = 1000
+export var reverse_speed = 150
+export var deceleration = 800
+export var rot_speed = 120
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
