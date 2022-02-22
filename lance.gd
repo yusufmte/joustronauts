a713c6ab99617d87f82734987bde64ad4efb6f08
Yusuf Ebrahim <yusufmte@gmail.com>
2022-02-21 19:12:15 -0500

    shorten decel function and rename to apply_friction

--- lance.gd
+++ lance.gd
@@ -4,7 +4,7 @@ export var acceleration = 400
 export var max_speed = 300
 export var reverse_acceleration = 200
 export var reverse_max_speed = -150
-export var deceleration = 100
+export var coef_of_friction = 100
 export var rotational_speed = 6.28
 
 var velocity = Vector2.ZERO
@@ -16,22 +16,18 @@ func _ready():
 # thrust when forward/back are pressed and below max speeds
 func thrust(delta):
 	var directional_speed = (velocity.x*cos(rotation-PI/2)) + (velocity.y*sin(rotation-PI/2)) #speed in the direction of the rotation of the lance
-	if Input.is_action_pressed("lanceP_forward") and directional_speed < max_speed:
-			velocity -= Vector2(0,1).rotated(rotation) * acceleration * delta
-	elif Input.is_action_pressed("lanceP_reverse") and directional_speed > reverse_max_speed:
+	if Input.is_action_pressed("lanceP_reverse") and directional_speed > reverse_max_speed:
 			velocity += Vector2(0,1).rotated(rotation) * acceleration * delta
+	elif Input.is_action_pressed("lanceP_forward") and directional_speed < max_speed:
+			velocity -= Vector2(0,1).rotated(rotation) * acceleration * delta
+
 
 # decelerate gradually until stopped
-func decelerate(delta):
-	if abs(velocity.y) > abs(sin(velocity.angle())) * deceleration * delta:
-		velocity.y -= sin(velocity.angle()) * deceleration * delta
+func apply_friction(delta):
+	if velocity.length() > (velocity.normalized() * coef_of_friction * delta).length():
+		velocity -= velocity.normalized() * coef_of_friction * delta
 	else:
-		velocity.y = 0
-
-	if abs(velocity.x) > abs(cos(velocity.angle())) * deceleration * delta:
-		velocity.x -= cos(velocity.angle()) * deceleration * delta
-	else:
-		velocity.x = 0
+		velocity = Vector2.ZERO
 
 # rotate when rotation keys are pressed
 func rotate(delta):
@@ -43,6 +39,6 @@ func rotate(delta):
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	thrust(delta)
-	decelerate(delta)
+	apply_friction(delta)
 	rotate(delta)
 	move_and_slide(velocity)
