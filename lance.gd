39071b3d9de00934b4dd3a057bc67aa83d9c166e
Yusuf Ebrahim <yusufmte@gmail.com>
2022-02-27 15:10:30 -0500

    added capability for second type of lance

--- lance.gd
+++ lance.gd
@@ -7,18 +7,32 @@ export var reverse_max_speed = -150
 export var coef_of_friction = 100
 export var rotational_speed = 6.28
 
+export var lance_type = ""
+var control_code = ""
+var orange_sketch = preload("res://lance_orange_sketch.png")
+var purple_sketch = preload("res://lance_purple_sketch.png")
+
 var velocity = Vector2.ZERO
+
+func set_lance_type(new_type):
+	lance_type = new_type
+	if lance_type == "purple":
+		$Sprite.texture = purple_sketch
+		control_code = "P"
+	elif lance_type == "orange":
+		$Sprite.texture = orange_sketch
+		control_code = "O"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	set_lance_type("orange")
 
 # thrust when forward/back are pressed and below max speeds
 func thrust(delta):
 	var directional_speed = (velocity.x*cos(rotation-PI/2)) + (velocity.y*sin(rotation-PI/2)) #speed in the direction of the rotation of the lance
-	if Input.is_action_pressed("lanceP_reverse") and directional_speed > reverse_max_speed:
+	if Input.is_action_pressed("lance"+control_code+"_reverse") and directional_speed > reverse_max_speed:
 			velocity += Vector2(0,1).rotated(rotation) * acceleration * delta
-	elif Input.is_action_pressed("lanceP_forward") and directional_speed < max_speed:
+	elif Input.is_action_pressed("lance"+control_code+"_forward") and directional_speed < max_speed:
 			velocity -= Vector2(0,1).rotated(rotation) * acceleration * delta
 
 
@@ -31,9 +45,9 @@ func apply_friction(delta):
 
 # rotate when rotation keys are pressed
 func rotate(delta):
-	if Input.is_action_pressed("lanceP_rot_CCW"):
+	if Input.is_action_pressed("lance"+control_code+"_rot_CCW"):
 		rotation -= rotational_speed * delta
-	if Input.is_action_pressed("lanceP_rot_CW"):
+	if Input.is_action_pressed("lance"+control_code+"_rot_CW"):
 		rotation += rotational_speed * delta
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
