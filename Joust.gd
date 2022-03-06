1b371a04722a36d1b56f85b9197a5b563af06e4d
Yusuf Ebrahim <yusufmte@gmail.com>
2022-03-06 15:23:01 -0500

    connected lanced signal to joust scene

--- Joust.gd
+++ Joust.gd
@@ -12,10 +12,15 @@ func _ready():
 	orange_lance.set_lance_type("orange")
 	orange_lance.set_position(orange_lance_start_pos)
 	orange_lance.name = "orange_lance"
+	orange_lance.connect("lanced",self,"_on_Lanced",["orange"])
 	add_child(purple_lance)
 	purple_lance.set_position(purple_lance_start_pos)
 	purple_lance.set_lance_type("purple")
 	purple_lance.name = "purple_lance"
+	purple_lance.connect("lanced",self,"_on_Lanced",["purple"])
+	
+func _on_Lanced(lanced_lance):
+	print("oh no, "+lanced_lance+" has been lanced!")
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
