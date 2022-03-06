26024a4533aae36f46f25018036f662d451b96b8
Yusuf Ebrahim <yusufmte@gmail.com>
2022-03-06 16:02:18 -0500

    joust scene obtains screen size on ready

--- Joust.gd
+++ Joust.gd
@@ -6,8 +6,11 @@ var purple_lance = lance.instance()
 var orange_lance_start_pos = Vector2(200,200)
 var purple_lance_start_pos = Vector2(800,500)
 
+var screen_size
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	screen_size = get_viewport_rect().size
 	
 	add_child(orange_lance)
 	orange_lance.set_lance_type("orange")
