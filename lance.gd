1536d55dfad2129da8bb0c8189d349efc3b703eb
Yusuf Ebrahim <yusufmte@gmail.com>
2022-03-06 13:32:32 -0500

    overhauled lance physics to improve interaction, changed lance type to RigidBody2D

--- lance.gd
+++ lance.gd
@@ -1,18 +1,17 @@
-extends KinematicBody2D
+extends RigidBody2D
 
-export var acceleration = 400
-export var max_speed = 300
-export var reverse_acceleration = 200
-export var reverse_max_speed = -150
-export var coef_of_friction = 100
-export var rotational_speed = 6.28
+export var acceleration = 2000
+export var max_speed = 4000
+export var reverse_acceleration = 150
+export var reverse_max_speed = -225
+export var coef_of_friction = 5
+export var rotational_speed = 50
+export var max_rotational_speed = 7
 
 export var lance_type = ""
 var control_code = ""
 var orange_sketch = preload("res://lance_orange_sketch.png")
 var purple_sketch = preload("res://lance_purple_sketch.png")
-
-var velocity = Vector2.ZERO
 
 func set_lance_type(new_type):
 	lance_type = new_type
@@ -25,34 +24,32 @@ func set_lance_type(new_type):
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	gravity_scale = 0
 	set_lance_type("orange")
 
 # thrust when forward/back are pressed and below max speeds
 func thrust(delta):
-	var directional_speed = (velocity.x*cos(rotation-PI/2)) + (velocity.y*sin(rotation-PI/2)) #speed in the direction of the rotation of the lance
+	var directional_speed = (linear_velocity.x*cos(rotation-PI/2)) + (linear_velocity.y*sin(rotation-PI/2)) #speed in the direction of the rotation of the lance
 	if Input.is_action_pressed("lance"+control_code+"_reverse") and directional_speed > reverse_max_speed:
-			velocity += Vector2(0,1).rotated(rotation) * acceleration * delta
+			apply_central_impulse(Vector2(0,1).rotated(rotation) * acceleration * delta)
 	elif Input.is_action_pressed("lance"+control_code+"_forward") and directional_speed < max_speed:
-			velocity -= Vector2(0,1).rotated(rotation) * acceleration * delta
+			apply_central_impulse(Vector2(0,1).rotated(rotation) * -1 * acceleration * delta)
 
-
-# decelerate gradually until stopped
+# decelerate according to current velocity
 func apply_friction(delta):
-	if velocity.length() > (velocity.normalized() * coef_of_friction * delta).length():
-		velocity -= velocity.normalized() * coef_of_friction * delta
-	else:
-		velocity = Vector2.ZERO
+	apply_central_impulse(linear_velocity * -1 * coef_of_friction * delta)
 
 # rotate when rotation keys are pressed
 func rotate(delta):
-	if Input.is_action_pressed("lance"+control_code+"_rot_CCW"):
-		rotation -= rotational_speed * delta
-	if Input.is_action_pressed("lance"+control_code+"_rot_CW"):
-		rotation += rotational_speed * delta
+	if Input.is_action_pressed("lance"+control_code+"_rot_CCW") and angular_velocity > max_rotational_speed * -1:
+		angular_velocity -= rotational_speed * delta
+	if Input.is_action_pressed("lance"+control_code+"_rot_CW") and angular_velocity < max_rotational_speed:
+		angular_velocity += rotational_speed * delta
+	if not Input.is_action_pressed("lance"+control_code+"_rot_CW") and not Input.is_action_pressed("lance"+control_code+"_rot_CCW"):
+		angular_velocity = 0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	thrust(delta)
 	apply_friction(delta)
 	rotate(delta)
-	move_and_slide(velocity)
